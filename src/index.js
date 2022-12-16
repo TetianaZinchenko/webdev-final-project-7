06986c598efa790a1a0e8e06ce6668801f873740
TetianaZinchenko <106535030+TetianaZinchenko@users.noreply.github.com>
2022-12-16 23:34:03 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-const goItBtn = document.querySelector('.footer-section__btn');
-const teamModal = document.querySelector('.backdrop');
-const modalBtn = document.querySelector('.modal-btn');
-
-goItBtn.addEventListener('click', openTeam);
-modalBtn.addEventListener('click', closeTeam);
-
-export function openTeam(e) {
-  e.preventDefault();
-  teamModal.classList.remove('is-hidden');
-}
-
-export function closeTeam(e) {
-  e.preventDefault();
-  teamModal.classList.add('is-hidden');
-}
+import './js/modal/team-modal';
