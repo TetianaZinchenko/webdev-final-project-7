d5bece4923fa5aedbe857166670f8909430a2b03
Volodymyr Lysenko <lusiy321@gmail.com>
2022-12-17 16:32:11 +0200

    finish modal form

--- src/js/modal/team-modal.js
+++ src/js/modal/team-modal.js
@@ -1,9 +1,10 @@
 const goItBtn = document.querySelector('.footer-section__btn');
 const teamModal = document.querySelector('.backdrop-team');
 const modalBtn = document.querySelector('.modal-team-btn');
-
+window.addEventListener('keydown', closeEscape);
 goItBtn.addEventListener('click', openTeam);
 modalBtn.addEventListener('click', closeTeam);
+teamModal.addEventListener('click', closeClick);
 
 export function openTeam(e) {
   e.preventDefault();
@@ -13,4 +14,18 @@ export function openTeam(e) {
 export function closeTeam(e) {
   e.preventDefault();
   teamModal.classList.add('is-hidden-team');
+}
+
+export function closeEscape(e) {
+  if (e.key === 'Escape') {
+    closeTeam(e);
+    return;
+  }
+}
+
+export function closeClick(e) {
+  if (e.target === teamModal) {
+    closeTeam(e);
+    return;
+  }
 }
