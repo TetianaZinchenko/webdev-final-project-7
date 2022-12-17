52337257aa1b054bd6761d695f16b5e30612dde5
antonina-kicha <107000832+antonina-kicha@users.noreply.github.com>
2022-12-17 17:10:51 +0000

    Deploying to gh-pages from @ TetianaZinchenko/webdev-final-project-7@f980ec908fb543dfa4bf529ecdc2b7ff4ae61bba 🚀

---
+++ index.6f36c58c.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".loader");window.addEventListener("load",(()=>{e.classList.add("fadeOut"),setTimeout((()=>{e.style.display="none"}),1e3)}));
+//# sourceMappingURL=index.6f36c58c.js.map
---
+++ index.b7834995.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".loader");window.addEventListener("load",(function(){e.classList.add("fadeOut"),setTimeout((function(){e.style.display="none"}),1e3)}))}();
+//# sourceMappingURL=index.b7834995.js.map
