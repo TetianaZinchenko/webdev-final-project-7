eb636b018adc07f8dcd1d366f95579c273a3045f
Antonina Kicha <antonina.kicha@gmail.com>
2022-12-18 22:53:43 +0200

    Добавляет рабочую модалку для фильма

--- src/js/modal/modal-for-movie.js
+++ src/js/modal/modal-for-movie.js
@@ -2,66 +2,94 @@
 const BASE_URL = "https://api.themoviedb.org/3/movie/";
 const apiKey = 'f42f2f62d598d39d316744d8859de3e9';
 
-const galleryListItem = document.querySelector('.gallery-list__item');
-galleryListItem.addEventListener('click', getId);
+// общие данные
+const galleryItem = document.querySelector('.gallery-js')
 
-function getId(evt) {
-    console.log(evt.currentTarget)
-}
+// данные для действий с модалкой
+const closeModalMovieBtn = document.querySelector('.btn-close-js');
+const backdrop = document.querySelector('.backdrop');
+const modal = document.querySelector('.modal-movie-detail');
 
-
-
-
-
-
-
-
+// елементы для создания разметки 
 const listRender = document.querySelector('.movie-detail-info-category-render');
-const about = document.querySelector('.about');
+const about = document.querySelector('.about-desc');
 const title = document.querySelector('.movie-title');
 const posterImg = document.querySelector('.thumb-movie-poster')
 
-// данные для действий с модалкой
+//получаем ИД фильма
+function getId(evt) {
+    if (evt.target.offsetParent.nodeName !== "LI") {
+        return;
+    }
+    return evt.target.offsetParent.dataset.id;
+}
 
-const closeModalMovieBtn = document.querySelector('.btn-close-js');
-const backdrop = document.querySelector('.backdrop');
-const galleryItem = document.querySelector('.gallery-js')
-const modal = document.querySelector('.modal-movie-detail');
-
+//открытые-рендер модалки
 galleryItem.addEventListener('click', openModal);
 
-let movieId;
-
-
-// function getIdMovie(evt) {
-//     console.log(evt.target)
-// }  
-
-function openModal(evt) {
-    evt.preventDefault();
-    console.dir(evt.target); // тут ищи как получить ИД
-
-    backdrop.classList.toggle("modal—movie-is-hidden")
-    getIdMovie();
-
+async function openModal(evt) {
+    let movieId = getId(evt);
+    if (!movieId) {
+        return
+    }
+    backdrop.classList.toggle("modal—movie-is-hidden"); //меняет видимость модалки
     
+    let data = await movieDatabaseApi(movieId); //отправляет запрос по ИД для получения данных о фильме
+    createMarkup(data);
+
     document.addEventListener("keydown", checkClick);
     document.addEventListener("click", checkClick);
     closeModalMovieBtn.addEventListener('click', checkClickBtn);
 }
 
+async function movieDatabaseApi(movieId) {
+    try {
+        const response = await fetch(`${BASE_URL}${movieId}?api_key=${apiKey}`);
+        if (!response.ok) {
+            throw new Error('RESP NOT OK!')
+        }
+        let data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+function createMarkup(data) {
+    posterImg.innerHTML = `<picture>
+          <source srcset="https://image.tmdb.org/t/p/w500${data.poster_path}" media="(min-width: 1280px)" />
+          <source srcset="https://image.tmdb.org/t/p/w300${data.poster_path}" media="(max-width: 1279px)" />
+         
+          <img src="https://image.tmdb.org/t/p/w154${data.poster_path}" alt="poster for movie" />
+        </picture> `;
+
+        title.innerHTML = `${data.title}`;
+        
+        const genresArr = data.genres;
+        const genres = genresArr.map(genre => genre.name).join(', ');
+
+        const markupList = `<li class="list-item"><span class="vote">${data.vote_average}</span> / <span class="votes">${data.vote_count}</span></li>
+        <li class="list-item">${data.popularity}</li>
+        <li class="list-item">${data.original_title}</li>
+        <li class="list-item">${genres}</li>`;
+
+        listRender.innerHTML = markupList;
+        about.innerHTML = `<p class="about-desc">
+        ${data.overview}
+      </p>`
+}
+
+ //закрытие модалки и удаление слушателей
 function checkClickBtn() {
     backdrop.classList.toggle("modal—movie-is-hidden");
     removeEventListener()
 }
 
 function checkClick(evt) {
-    if (evt.code === "Escape") {
+    if (evt.code === "Escape" || evt.target.className === "backdrop") {
         backdrop.classList.toggle("modal—movie-is-hidden");
         removeEventListener()
-    } else if(evt.target.className === "backdrop"){
-        backdrop.classList.toggle("modal—movie-is-hidden");
-         removeEventListener()
     }
 }
 
@@ -71,40 +99,3 @@ function removeEventListener() {
     closeModalMovieBtn.removeEventListener("click", checkClickBtn)
 }
 
-
-async function movieDatabaseApi() {
-    try {
-        const response = await fetch(`${BASE_URL}${movieId}?api_key=${apiKey}`);
-        const data = await response.json();
-
-        posterImg.insertAdjacentHTML('beforeend', `<picture>
-          <source srcset="https://image.tmdb.org/t/p/w500${data.poster_path}" media="(min-width: 1280px)" />
-          <source srcset="https://image.tmdb.org/t/p/w300${data.poster_path}" media="(max-width: 1279px)" />
-         
-          <img src="https://image.tmdb.org/t/p/w154${data.poster_path}" alt="poster for movie" />
-        </picture> `)
-
-        title.insertAdjacentHTML('beforeend', `${data.title}`)
-
-        const genres = data.genres;
-        const genresArr = genres.map(genre => genre.name).join(', ');
-
-        const markupList = `<li class="list-item"><span class="vote">${data.vote_average}</span> / <span class="votes">${data.vote_count}</span></li>
-        <li class="list-item">${data.popularity}</li>
-        <li class="list-item">${data.original_title}</li>
-        <li class="list-item">${genresArr}</li>`;
-                console.log(data.overview);
-
-        listRender.insertAdjacentHTML('beforeend', markupList);
-              about.insertAdjacentHTML('afterend', `<p class="about-desc">
-        ${data.overview}
-      </p>`)
-
-    }
-    catch {
-        
-    }
-}
-
-
-// movieDatabaseApi()
