b2e77e27dedcc7ab2b0919e6624249284f98abb6
TetianaZinchenko <106535030+TetianaZinchenko@users.noreply.github.com>
2022-12-20 22:01:58 +0000

    Deploying to gh-pages from @ TetianaZinchenko/webdev-final-project-7@50682c8b4a4f12222a086ff07417d0b19e940079 🚀

--- library.2774b16f.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".loader");window.addEventListener("load",(()=>{e.classList.add("fadeOut"),setTimeout((()=>{e.style.display="none"}),1e3)}));
-//# sourceMappingURL=library.2774b16f.js.map
--- library.42105202.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".loader");window.addEventListener("load",(function(){e.classList.add("fadeOut"),setTimeout((function(){e.style.display="none"}),1e3)}))}();
-//# sourceMappingURL=library.42105202.js.map
